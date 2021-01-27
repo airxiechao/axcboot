0a1bcb1746141244d89d865bbad6a11fb2b96392
airxiechao@qq.com <airxiechao@qq.com>
2021-01-27 20:29:41 +0800

    update rpc mixin

--- src/main/java/com/airxiechao/axcboot/communication/rpc/client/RpcClient.java
+++ src/main/java/com/airxiechao/axcboot/communication/rpc/client/RpcClient.java
@@ -9,14 +9,14 @@ import com.airxiechao.axcboot.communication.rpc.util.RpcUtil;
 import com.alibaba.fastjson.JSON;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.channel.ChannelHandler.Sharable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,9 +27,14 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.airxiechao.axcboot.communication.rpc.common.RpcContext.HEARTBEAT_PERIOD_SECS;
+import static com.airxiechao.axcboot.communication.rpc.util.RpcUtil.buildResponseType;
 
+/**
+ * rpc client
+ */
 public class RpcClient {
 
     private static final Logger logger = LoggerFactory.getLogger(RpcClient.class);
@@ -147,7 +152,7 @@ public class RpcClient {
         clientGroup = new NioEventLoopGroup(this.numIoThreads);
         clientBootstrap.group(clientGroup);
         RpcMessageEncoder encoder = new RpcMessageEncoder();
-        router = new RpcClientMessageRouter(this);
+        router = new RpcClientMessageRouter();
         clientBootstrap.channel(NioSocketChannel.class).handler(new ChannelInitializer<SocketChannel>() {
             @Override
             protected void initChannel(SocketChannel ch) throws Exception {
@@ -375,7 +380,7 @@ public class RpcClient {
         try{
             String requestId = RequestId.next();
             RpcMessage message = new RpcMessage(requestId, type, payload);
-            RpcFuture future = this.router.sendToServer(message);
+            RpcFuture future = router.sendToServer(message);
             if(null != callTimeoutSecs){
                 resp = future.get(callTimeoutSecs, TimeUnit.SECONDS);
             }else{
@@ -394,7 +399,7 @@ public class RpcClient {
      * @return
      */
     public boolean isServerActive(){
-        RpcContext ctx = this.router.getRpcContext();
+        RpcContext ctx = router.getRpcContext();
         if(null != ctx && !ctx.isHeartbeatExpired()){
             return true;
         }
@@ -410,32 +415,258 @@ public class RpcClient {
         this.status = status;
     }
 
-    public String getName(){
-        return this.name;
-    }
-
-    public int getReconnectDelaySecs(){
-        return this.reconnectDelaySecs;
-    }
-
-    public Map<String, IRpcMessageHandler> getServiceHandlers(){
-        return serviceHandlers;
-    }
-
-    public int getNumWorkerThreads(){
-        return numWorkerThreads;
-    }
-
-    public IRpcAuthChecker getAuthChecker(){
-        return authChecker;
-    }
-
-    public boolean isVerboseLog() {
-        return verboseLog;
-    }
-
     public RpcClient setVerboseLog(boolean verboseLog) {
         this.verboseLog = verboseLog;
         return this;
     }
+
+
+    /***********************************************************************************************************
+     *
+     *                                           rpc client router mixin
+     *
+     ***********************************************************************************************************/
+
+
+
+    @Sharable
+    public class RpcClientMessageRouter extends ChannelInboundHandlerAdapter {
+
+        private ThreadPoolExecutor executor;
+        private RpcContext rpcContext = new RpcContext();
+        private Map<String, RpcFuture> pendingRequests = new ConcurrentHashMap<>();
+        private boolean shouldReconnect = true;
+
+        public RpcClientMessageRouter(){
+
+            BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(1000);
+            ThreadFactory factory = new ThreadFactory() {
+
+                AtomicInteger seq = new AtomicInteger();
+
+                @Override
+                public Thread newThread(Runnable r) {
+                    Thread t = new Thread(r);
+                    t.setName("rpc-client["+name+"]-" + seq.getAndIncrement());
+                    t.setDaemon(true);
+                    return t;
+                }
+
+            };
+
+            this.executor = new ThreadPoolExecutor(numWorkerThreads, numWorkerThreads * 100, 30, TimeUnit.SECONDS,
+                    queue, factory, new ThreadPoolExecutor.CallerRunsPolicy());
+
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            updateRpcContext(ctx, new Date());
+
+            heartbeatOnce();
+            runConnectListener();
+        }
+
+        @Override
+        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+            closeConection();
+
+            // 尝试重连
+            if(this.shouldReconnect){
+                ctx.channel().eventLoop().schedule(() -> {
+                    connect();
+                }, reconnectDelaySecs, TimeUnit.SECONDS);
+            }
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            if (!(msg instanceof RpcMessage)) {
+                return;
+            }
+
+            RpcMessage message = (RpcMessage) msg;
+
+            if(message.isResponse()){
+                this.handleResponseMessage(ctx, message);
+            }else{
+                this.executor.execute(() -> {
+                    this.handleServiceMessage(ctx, message);
+                });
+            }
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            ctx.close();
+
+            if(cause.getCause() instanceof SSLHandshakeException){
+                this.shouldReconnect = false;
+            }
+
+            logger.error("close rpc-client-[{}] to server connection by uncaught error", name, verboseLog ? cause : null);
+        }
+
+        /**
+         * 处理响应消息
+         * @param ctx
+         * @param message
+         * @throws Exception
+         */
+        private void handleResponseMessage(ChannelHandlerContext ctx, RpcMessage message) throws Exception {
+            RpcFuture future = pendingRequests.remove(message.getRequestId());
+            if (future == null) {
+                logger.error("rpc future not found with type {}", message.getType());
+                return;
+            }
+
+            try{
+                Response response = JSON.parseObject(message.getPayload(), Response.class);
+                future.success(response);
+            }catch (Exception e){
+                logger.error("parse rpc response message error", e);
+                future.fail(e);
+
+                throw new Exception("parse rpc response message error", e);
+            }
+        }
+
+        /**
+         * 处理请求消息
+         * @param ctx
+         * @param message
+         */
+        private void handleServiceMessage(ChannelHandlerContext ctx, RpcMessage message){
+
+            if(verboseLog) {
+                logAccess(ctx, message);
+            }
+
+            IRpcMessageHandler handler = null;
+            if(null != message.getType()){
+                handler = serviceHandlers.get(message.getType());
+            }
+
+            Response response;
+            if(null != handler){
+                try {
+                    String payload = message.getPayload();
+                    Map payloadMap = JSON.parseObject(payload, Map.class);
+
+                    RpcUtil.checkAuth(handler, ctx, payloadMap, authChecker);
+                    RpcUtil.checkParameter(handler, payloadMap);
+                    response = handler.handle(ctx, payloadMap);
+                }catch (Exception e){
+                    logger.error("handle rpc service [{}] error", message.getType(), e);
+
+                    response = new Response();
+                    response.error(e.getMessage());
+                }
+            }else{
+                response = new Response();
+                response.error("no rpc service [" + message.getType() + "]");
+            }
+
+            ctx.writeAndFlush(new RpcMessage(message.getRequestId(), buildResponseType(message.getType()), JSON.toJSONString(response)));
+        }
+
+        /**
+         * 向服务端发送rpc消息
+         * @param message
+         * @return
+         */
+        public RpcFuture sendToServer(RpcMessage message) {
+            ChannelHandlerContext ctx = getActiveChannelHandlerContext();
+
+            RpcFuture future = new RpcFuture();
+            ChannelHandlerContext ctx1 = ctx;
+            if (ctx != null) {
+                ctx.channel().eventLoop().execute(() -> {
+                    pendingRequests.put(message.getRequestId(), future);
+                    ctx1.writeAndFlush(message);
+                });
+            } else {
+                future.fail(new Exception("rpc-client-["+name+"] connection not active error"));
+            }
+            return future;
+        }
+
+
+        /**
+         * 关闭连接
+         */
+        public void closeConection() {
+            pendingRequests.forEach((__, future) -> {
+                future.fail(new Exception("rpc-client-["+name+"] connection not active error"));
+            });
+            pendingRequests.clear();
+
+            ChannelHandlerContext ctx = rpcContext.getContext();
+            if (ctx != null) {
+                ctx.close();
+            }
+            updateRpcContext(null, null);
+
+            setStatus(RpcClient.RPC_CLIENT_STATUS.NOT_CONNECTED);
+        }
+
+        /**
+         * 关闭
+         */
+        public void closeGracefully() {
+            closeConection();
+
+            this.executor.shutdown();
+            try {
+                this.executor.awaitTermination(10, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+            }
+            this.executor.shutdownNow();
+        }
+
+        public RpcContext getRpcContext(){
+            return rpcContext;
+        }
+
+        /**
+         * 更新rpc上下文
+         * @param ctx
+         * @param lastHeartbeatTime
+         */
+        public void updateRpcContext(ChannelHandlerContext ctx, Date lastHeartbeatTime){
+            rpcContext.setContext(ctx);
+            updateRpcContextLastHeartbeatTime(lastHeartbeatTime);
+        }
+
+        public void updateRpcContextLastHeartbeatTime(Date lastHeartbeatTime){
+            if(null != lastHeartbeatTime){
+                rpcContext.setLastHeartbeatTime(lastHeartbeatTime);
+            }
+        }
+
+        /**
+         * 获取活跃通道上下文
+         * @return
+         */
+        private ChannelHandlerContext getActiveChannelHandlerContext(){
+            ChannelHandlerContext ctx = null;
+            if(!rpcContext.isHeartbeatExpired()){
+                ctx = rpcContext.getContext();
+            }
+
+            return ctx;
+        }
+
+        /**
+         * 记录请求
+         * @param ctx
+         * @param message
+         */
+        private void logAccess(ChannelHandlerContext ctx, RpcMessage message){
+            String clientIp = RpcUtil.getRemoteIp(ctx);
+            logger.info("->> rpc request [{}] from [{}]", message.getType(), clientIp);
+        }
+
+    }
+
 }
--- src/main/java/com/airxiechao/axcboot/communication/rpc/server/RpcServer.java
+++ src/main/java/com/airxiechao/axcboot/communication/rpc/server/RpcServer.java
@@ -6,6 +6,7 @@ import com.airxiechao.axcboot.communication.common.annotation.Query;
 import com.airxiechao.axcboot.communication.rpc.common.*;
 import com.airxiechao.axcboot.communication.rpc.security.IRpcAuthChecker;
 import com.airxiechao.axcboot.communication.rpc.util.RpcUtil;
+import com.airxiechao.axcboot.util.UuidUtil;
 import com.alibaba.fastjson.JSON;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
@@ -16,6 +17,7 @@ import io.netty.handler.ssl.ClientAuth;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.channel.ChannelHandler.Sharable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,8 +25,15 @@ import javax.net.ssl.*;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
+import static com.airxiechao.axcboot.communication.rpc.util.RpcUtil.buildResponseType;
+
+/**
+ * rpc server
+ */
 public class RpcServer {
     private static final Logger logger = LoggerFactory.getLogger(RpcServer.class);
 
@@ -129,7 +138,7 @@ public class RpcServer {
         serverGroup = new NioEventLoopGroup(this.numIoThreads);
         serverBootstrap.group(serverGroup);
         RpcMessageEncoder encoder = new RpcMessageEncoder();
-        router = new RpcServerMessageRouter(this);
+        router = new RpcServerMessageRouter();
         serverBootstrap.channel(NioServerSocketChannel.class).childHandler(
                 new ChannelInitializer<SocketChannel>() {
                     @Override
@@ -268,7 +277,7 @@ public class RpcServer {
      */
     private void registerHeartbeatHandler(){
         registerRpcHandler(RpcMessage.TYPE_PING, (ctx, payload) -> {
-            String clientName = getRouter().getClientByContext(ctx);
+            String clientName = router.getClientByContext(ctx);
             String name = (String)payload.get("name");
 
             logger.info("rpc-server-[{}] receives heartbeat from rpc-client-[{}, {}]", this.name, name, clientName);
@@ -313,7 +322,7 @@ public class RpcServer {
         try{
             String requestId = RequestId.next();
             RpcMessage message = new RpcMessage(requestId, type, payload);
-            RpcFuture future = this.router.sendToClient(client, message);
+            RpcFuture future = router.sendToClient(client, message);
             if(null != callTimeoutSecs){
                 resp = future.get(callTimeoutSecs, TimeUnit.SECONDS);
             }else{
@@ -354,11 +363,6 @@ public class RpcServer {
         return false;
     }
 
-
-    public String getName(){
-        return this.name;
-    }
-
     public RPC_SERVER_STATUS getStatus(){
         return status;
     }
@@ -367,28 +371,355 @@ public class RpcServer {
         this.status = status;
     }
 
-    public Map<String, IRpcMessageHandler> getServiceHandlers(){
-        return serviceHandlers;
-    }
-
-    public int getNumWorkerThreads(){
-        return numWorkerThreads;
-    }
-
-    public IRpcAuthChecker getAuthChecker(){
-        return authChecker;
-    }
-
     public RpcServerMessageRouter getRouter(){
-        return this.router;
-    }
-
-    public boolean isVerboseLog() {
-        return verboseLog;
+        return router;
     }
 
     public RpcServer setVerboseLog(boolean verboseLog) {
         this.verboseLog = verboseLog;
         return this;
+    }
+
+
+
+    /**************************************************************************************************************
+     *
+     *                                          rpc server router mixin
+     *
+     **************************************************************************************************************/
+
+
+
+    @Sharable
+    public class RpcServerMessageRouter extends ChannelInboundHandlerAdapter {
+
+        private ThreadPoolExecutor executor;
+        private Map<String, RpcContext> rpcContexts = new ConcurrentHashMap<>();
+        private Map<String, RpcClientFuture> pendingRequests = new ConcurrentHashMap<>();
+
+        public RpcServerMessageRouter(){
+
+            BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(1000);
+            ThreadFactory factory = new ThreadFactory() {
+
+                AtomicInteger seq = new AtomicInteger();
+
+                @Override
+                public Thread newThread(Runnable r) {
+                    Thread t = new Thread(r);
+                    t.setName("rpc-server-["+name+"]-" + seq.getAndIncrement());
+                    t.setDaemon(true);
+                    return t;
+                }
+
+            };
+
+            this.executor = new ThreadPoolExecutor(numWorkerThreads, numWorkerThreads * 100, 30, TimeUnit.SECONDS,
+                    queue, factory, new ThreadPoolExecutor.CallerRunsPolicy());
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+
+            String clientName = UuidUtil.random();
+
+            logger.info("rpc-client-[{}] active", clientName);
+
+            // 如果已经存在相同名称的其他客户端，则断开连接
+            updateOrCloseRpcContext(clientName, ctx);
+        }
+
+        @Override
+        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+
+            String client = getClientByContext(ctx);
+
+            logger.info("rpc-client-[{}] inactive", client);
+
+            if(null != client){
+                clearClient(client);
+            }
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception{
+            if (!(msg instanceof RpcMessage)) {
+                return;
+            }
+
+            RpcMessage message = (RpcMessage) msg;
+
+            if(message.isResponse()){
+                this.handleResponseMessage(ctx, message);
+            }else{
+                this.executor.execute(() -> {
+                    this.handleServiceMessage(ctx, message);
+                });
+            }
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            ctx.close();
+
+            String client = getClientByContext(ctx);
+            logger.error("close rpc-client-[{}] connection by uncaught error", client, verboseLog ? cause : null);
+        }
+
+        /**
+         * 处理响应消息
+         * @param ctx
+         * @param message
+         */
+        private void handleResponseMessage(ChannelHandlerContext ctx, RpcMessage message) throws Exception {
+            RpcClientFuture clientFuture = pendingRequests.remove(message.getRequestId());
+            if (clientFuture == null) {
+                logger.error("rpc future not found with type {}", message.getType());
+                return;
+            }
+
+            try{
+                Response response = JSON.parseObject(message.getPayload(), Response.class);
+                clientFuture.success(response);
+            }catch (Exception e){
+                logger.error("parse rpc response message error", e);
+                clientFuture.fail(e);
+
+                throw new Exception("parse rpc response message error", e);
+            }
+        }
+
+        /**
+         * 处理请求消息
+         * @param ctx
+         * @param message
+         */
+        private void handleServiceMessage(ChannelHandlerContext ctx, RpcMessage message){
+
+            if(verboseLog){
+                logAccess(ctx, message);
+            }
+
+            IRpcMessageHandler handler = null;
+            if(null != message.getType()){
+                handler = serviceHandlers.get(message.getType());
+            }
+
+            Response response;
+            if(null != handler){
+                try {
+                    String payload = message.getPayload();
+                    Map payloadMap = JSON.parseObject(payload, Map.class);
+
+                    RpcUtil.checkAuth(handler, ctx, payloadMap, authChecker);
+                    RpcUtil.checkParameter(handler, payloadMap);
+                    response = handler.handle(ctx, payloadMap);
+                }catch (Exception e){
+                    logger.error("handle rpc service [{}] error", message.getType(), e);
+
+                    response = new Response();
+                    response.error(e.getMessage());
+                }
+            }else{
+                response = new Response();
+                response.error("no rpc service [" + message.getType() + "]");
+            }
+
+            ctx.writeAndFlush(new RpcMessage(message.getRequestId(), buildResponseType(message.getType()), JSON.toJSONString(response)));
+        }
+
+        /**
+         * 向客户端发送rpc消息
+         * @param client
+         * @param message
+         * @return
+         */
+        public RpcFuture sendToClient(String client, RpcMessage message) {
+
+            ChannelHandlerContext ctx = null;
+            RpcContext rpcContext = getClientRpcContext(client);
+            if(null != rpcContext){
+                if(!rpcContext.isHeartbeatExpired()){
+                    ctx = rpcContext.getContext();
+                }
+            }
+
+            RpcClientFuture clientFuture = new RpcClientFuture();
+            ChannelHandlerContext ctx1 = ctx;
+            if (ctx1 != null) {
+
+                clientFuture.setClientName(client);
+                ctx1.channel().eventLoop().execute(() -> {
+                    pendingRequests.put(message.getRequestId(), clientFuture);
+                    ctx1.writeAndFlush(message);
+                });
+            } else {
+                clientFuture.fail(new Exception("rpc-client-["+client+"] connection not active"));
+            }
+            return clientFuture;
+        }
+
+        /**
+         * 关闭
+         */
+        public void closeGracefully() {
+            this.executor.shutdown();
+            try {
+                this.executor.awaitTermination(10, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+            }
+            this.executor.shutdownNow();
+        }
+
+        /**
+         * 清理客户端
+         * @param client
+         */
+        private void clearClient(String client){
+            List<String> futureKeys = pendingRequests.entrySet().stream()
+                    .filter(c->client.equals(c.getValue().getClientName()))
+                    .map(c->c.getKey())
+                    .collect(Collectors.toList());
+
+            for(String key : futureKeys){
+                RpcClientFuture future = pendingRequests.get(key);
+                future.fail(new Exception("rpc-client-["+client+"] connection not active error"));
+                pendingRequests.remove(key);
+            }
+
+            updateRpcContext(client, null, null);
+        }
+
+
+        /**
+         * 更新rpc上下文
+         * @param client
+         * @param ctx
+         * @param date
+         */
+        public void updateRpcContext(String client, ChannelHandlerContext ctx, Date date){
+
+            if(null == ctx && null == date){
+                removeClientRpcContext(client);
+                return;
+            }
+
+            RpcContext rpcContext = getClientRpcContext(client);
+            if(null == rpcContext){
+                rpcContext = new RpcContext();
+            }
+
+            rpcContext.setContext(ctx);
+            if(null != date){
+                rpcContext.setLastHeartbeatTime(date);
+            }
+
+            this.rpcContexts.put(client, rpcContext);
+        }
+
+        /**
+         * 获取客户端rpc上下文
+         * @param client
+         * @return
+         */
+        public RpcContext getClientRpcContext(String client){
+            RpcContext rpcContext = this.rpcContexts.get(client);
+            return rpcContext;
+        }
+
+        /**
+         * 删除客户端rpc上下文
+         * @param client
+         */
+        private void removeClientRpcContext(String client){
+            if(this.rpcContexts.containsKey(client)){
+                this.rpcContexts.remove(client);
+            }
+        }
+
+        /**
+         * 获取在线客户端
+         * @return
+         */
+        public List<String> getActiveClients(){
+            List<String> clients = new ArrayList<>();
+            for(Map.Entry<String, RpcContext> entry : rpcContexts.entrySet()){
+                String name = entry.getKey();
+                RpcContext context = entry.getValue();
+
+                if(!context.isHeartbeatExpired()){
+                    clients.add(name);
+                }
+            }
+            return clients;
+        }
+
+        /**
+         * 获取客户端
+         * @param ctx
+         * @return
+         */
+        public String getClientByContext(ChannelHandlerContext ctx){
+            Optional<String> opt = rpcContexts.entrySet().stream()
+                    .filter(c->ctx.equals(c.getValue().getContext()))
+                    .map(c->c.getKey())
+                    .findFirst();
+
+            String client = null;
+            if(opt.isPresent()) {
+                client = opt.get();
+            }
+
+            return client;
+        }
+
+        /**
+         * 查是否已经存在这个名称的其他客户端，并且没有过期
+         * @param ctx
+         * @param clientName
+         * @return
+         */
+        public boolean hasOtherClientWithName(ChannelHandlerContext ctx, String clientName){
+            if(null != clientName){
+                RpcContext rpcContext = getClientRpcContext(clientName);
+                if(null != rpcContext && !rpcContext.isHeartbeatExpired()){
+                    ChannelHandlerContext channelHandlerContext = rpcContext.getContext();
+                    if(null != channelHandlerContext){
+                        if(!ctx.equals(channelHandlerContext)){
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /**
+         * 更新rpc上下文，如果已经存在相同名称的其他客户端，则断开连接
+         * @param clientName
+         * @param ctx
+         */
+        public void updateOrCloseRpcContext(String clientName, ChannelHandlerContext ctx) throws Exception {
+            boolean has = hasOtherClientWithName(ctx, clientName);
+            if(has){
+                ctx.close();
+                logger.error("client [{}] already exists", clientName);
+                throw new Exception("client ["+clientName+"] already exists");
+            }
+
+            updateRpcContext(clientName, ctx, new Date());
+        }
+
+        /**
+         * 记录请求
+         * @param ctx
+         * @param message
+         */
+        private void logAccess(ChannelHandlerContext ctx, RpcMessage message){
+            String client = getClientByContext(ctx);
+            String clientIp = RpcUtil.getRemoteIp(ctx);
+            logger.info("->> rpc request [{}] from [client:{}, ip:{}]", message.getType(), client, clientIp);
+        }
     }
 }
