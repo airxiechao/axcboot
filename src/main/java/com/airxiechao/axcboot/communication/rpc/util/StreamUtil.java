9def2e09f7ddb3fc91eac2a09bbd4de7dc5e3350
airxiechao@qq.com <airxiechao@qq.com>
2021-02-01 23:10:16 +0800

    update

--- src/main/java/com/airxiechao/axcboot/communication/rpc/util/StreamUtil.java
+++ src/main/java/com/airxiechao/axcboot/communication/rpc/util/StreamUtil.java
@@ -3,6 +3,7 @@ package com.airxiechao.axcboot.communication.rpc.util;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.nio.charset.Charset;
 import java.util.function.Consumer;
 
 public class StreamUtil {
@@ -10,8 +11,8 @@ public class StreamUtil {
     /**
      * 读取输入流
      */
-    public static void readStringInputStream(InputStream inputStream, int bufferSize, String charsetName, Consumer<String> consumer) throws Exception {
-        try(Reader logReader = new InputStreamReader(inputStream, charsetName)){
+    public static void readStringInputStream(InputStream inputStream, int bufferSize, Charset charset, Consumer<String> consumer) throws Exception {
+        try(Reader logReader = new InputStreamReader(inputStream, charset)){
             char[] buffer = new char[bufferSize];
             while(true){
                 int num = logReader.read(buffer);
